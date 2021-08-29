d2270634b9a88aa3ffdeee6bf729220f41e67916
Ranur-react <rahmatnur845@gmail.com>
2021-08-29 22:01:47 +0700

    Kamera Motor Masuk udah mntap

--- DISPENSER/tutup.php
+++ DISPENSER/tutup.php
@@ -12,19 +12,19 @@ $d['id_mesin']=02;
 
 // 1. Bunyikan Suara "Terimakasih" Mesin
 // 2. Generate ID Parkir
-$id= GenerateID_Entry($d['id_mesin']);
-$d['id_parkir']=$id;
+		$id= GenerateID_Entry($d['id_mesin']);
+		//$d['id_parkir']=$id;
 // 3. Simpan ID Parkir
-mysqli_query($conn, "INSERT INTO `tb_entry` (`id_entry`, `gate_id`,date) VALUES ('$id', '02',now());");
+		//mysqli_query($conn, "INSERT INTO `tb_entry` (`id_entry`, `gate_id`,date) VALUES ('$id', '02',now());");
 // 4. Print Barcode
-PrintBarcode($id,$connfig);
+ 		//PrintBarcode($id,$connfig);
 // 5. Setelah Driver Mengambil Kertas Barcode, Controller Membuka Gate (php Write ='OUT1ON')
-Write($connfig,'TRIG1');
+		Write($connfig,'TRIG2');
 // Write($connfig,'OUT1ON');
-Write($connfig,'MT00002');
+		//Write($connfig,'MT00002');
 $CameraConfig['folder']=$id;
 
  CaptureImages($CameraConfig,$id);
 echo "_start_";
-echo "true";
+echo "Tutup";
 ?>
